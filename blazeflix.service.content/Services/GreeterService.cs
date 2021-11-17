fedac87fc3e3347d9ad9ea6839f2262b8d8b8d97
Gianluigi Conti <gianluigi.conti@outlook.com>
2021-11-17 22:39:36 +0100

    Add content grpc service from template

---
+++ blazeflix.service.content/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using blazeflix.service.content;
+using Grpc.Core;
+
+namespace blazeflix.service.content.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
